3f784a70bae4d86d177453ed024d223b3d8204e9
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-29 09:21:29 -0500

    commit tree 0cfe28fa37c30f7057c0cb6f36e046158a247db8

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const listening = document.getElementById('button')
+    listening.addEventListener('click', function(){
+        alert('i was clicked')    
+    })
 }
